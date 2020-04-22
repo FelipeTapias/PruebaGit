7d9b63bc16d6198c8b704ba03d4db560a52a9326
FelipeTapias <62154763+FelipeTapias@users.noreply.github.com>
2020-04-22 13:19:11 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
  
 app.set('port', 7000)
 console.log("lololololololol")
+console.log("holaaa")
  
 app.listen(app.get('port'), () => {
     console.log(`Eschuchando en puerto ${app.get('port')}`);
