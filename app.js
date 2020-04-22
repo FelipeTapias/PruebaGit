3bed739eec66f49b996485cc5d5367bdc038a620
Felipe Tapias <tapiastuberquiados>
2020-04-22 13:15:51 -0500

    Segundo commit prueba

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express()
  
 app.set('port', 7000)
+console.log("lololololololol")
  
 app.listen(app.get('port'), () => {
     console.log(`Eschuchando en puerto ${app.get('port')}`);
